9c93ce73d49a1dcaac1ae59812955da900fb04d1
burak <burak@DESKTOP-IRI7LEB>
2022-04-24 21:00:51 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,27 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] vs)
+        {
+            ProductManager productManager = new ProductManager(new InMemoryProductDal());
+
+            foreach (var product in productManager.GetAll())
+            {
+                Console.WriteLine(product.ProductName);
+            }
+            
+            
+                
+                
+                
+
+        }
+    }
+}
+
+
---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    //Public yapıldığı zaman bu class'a diğer katmanlar da ulaşabilecek
+    public class Product : IEntity
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+
+        public string ProductName { get; set; }
+        public short UnitsInStock { get; set; }
+        public decimal UnitPrice { get; set; }
+
+
+    }
+}
