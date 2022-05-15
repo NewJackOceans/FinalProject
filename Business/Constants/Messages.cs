043642bc070f6d4ec1080f922e94d423599c1a5a
burak karacakaya <burakaracakaya@gmail.com>
2022-05-15 22:52:31 +0300

    15.05.2022 23:00 Ders sonu

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,8 @@ namespace Business.Constants
     {
         public static string ProductAdded = "Ürün eklendi.";
         public static string ProductNameInvalid = "Ürün ismi geçersiz.";
+        public static string MaintananceTime = "Sistem bakımda";
+        public static string ProductListed = "Ürünler listelendi.";
 
     }
 }
