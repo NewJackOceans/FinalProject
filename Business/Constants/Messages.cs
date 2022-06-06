a4425cac3d54ceb2f779c851a14b3c6a1b118755
burak karacakaya <burakaracakaya@gmail.com>
2022-06-06 19:22:55 +0300

    06.06.2022 14.ders sonu

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -16,5 +16,14 @@ namespace Business.Constants
         public static string productCountOfCategoryError = "Bir kategoride en fazla 10 ürn olabilir.";
         public static string ProductNameAlreadyExists = "Bu isimde zaten başka bir ürün var.";
         public static string CategoryLimitExceded = "Kategori limiti aşıldığı için yeni ürün eklenemiyor.";
+        public static string AuthorizationDenied = "Yetkilendirme başarısız."; 
+        public static string UserRegistered = "Kullanıcı kaydı başarılı";
+        public static string UserNotFound = "Kullanıcı bulunamadı.";
+        public static string PasswordError = "Parola hatalı.";
+        public static string SuccessfulLogin = "Giriş başarılı.";
+        public static string UserAlreadyExists = "Kullanıcı zaten kayıtlı.";
+        public static string AccessTokenCreated = "Şifre oluşturuldu.";
+
+
     }
 }
